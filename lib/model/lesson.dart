c5e483d9e2ea1629cb30059e9185068736a481b9
MMAlamdarfard <alamdarmahdi99@gmail.com>
2023-07-25 15:58:08 +0330

    v1.0.0

---
+++ lib/model/lesson.dart
@@ -0,0 +1,16 @@
+class Lesson {
+  final String title;
+  final String duration;
+  final String videoPath;
+  Lesson(this.title, this.duration, this.videoPath);
+}
+
+List<Lesson> lessonsModel = [
+  Lesson("State Managment", "5:40", "videoPath"),
+  Lesson("Ui design", "2:40", "videoPath"),
+  Lesson("State Managment", "5:40", "videoPath"),
+  Lesson("Ui design", "2:40", "videoPath"),
+  Lesson("State Managment", "5:40", "videoPath"),
+  Lesson("Ui design", "2:40", "videoPath"),
+  Lesson("GetX tuturial", "5:40", "videoPath"),
+];
